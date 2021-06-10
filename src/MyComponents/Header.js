0c586915ee9fefe995c5869443b400ee590613d5
mdakram09 <iamakramkhan09@gmail.com>
2021-06-10 20:47:48 +0530

    adding contact Us page and everything is working fine

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -23,7 +23,7 @@ const Header = (props) => {
                                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/about">Link</Link>
+                                <Link className="nav-link" to="/about">About Us</Link>
                             </li>
                             <li className="nav-item dropdown">
                                 <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
